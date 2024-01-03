0a020c409ffddafb99c9b2e8444f47591d2922ce
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-03 23:15:36 +0200

    working version on windows

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::io::{Read, Write};
 use std::sync::{Arc, Mutex};
-use serialport::{SerialPort, SerialPortBuilder};
+use serialport::{self, SerialPort, SerialPortBuilder, SerialPortType, SerialPortInfo};
 use clap::Parser;
-
+use anyhow::{Result, bail, Context};
+use env_logger;
+use log::{debug, info};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
@@ -12,29 +14,67 @@ struct Args {
     port: Option<String>,
 
     /// port name (eg. COM23 OR /dev/ttyUSB0)
-    #[arg(short, long)]
+    #[arg(short, long, default_value = "115200")]
     baud_rate: Option<u32>,
+
+    /// product ID (as hex value, eg: 000a)
+    /// find automatically based on pid of usb
+    #[arg(long)]
+    product_id: Option<String>, 
 }
 
-fn do_main() -> Result<(), ()> {
+fn find_by_product_id(args: &Args) -> Result<Option<SerialPortInfo>> {
+    let ports = serialport::available_ports().unwrap();
+    for port in ports {
+        let port_clone = port.clone();
+        match port.port_type {
+            SerialPortType::UsbPort(info) => {
+                let pid = format!("{:04x}", info.pid);
+                if args.product_id.clone().unwrap() == pid {
+                    return Ok(Some(port_clone))
+                }
+            }
+            _ => {}
+        }
+    }
+    Ok(None)
+}
+
+fn open_serial_port(args: &Args) -> Result<SerialPort> {
+    let port_name = if args.product_id.is_some() {
+        find_by_product_id(&args)?
+            .map(|port_info| port_info.port_name)
+    } else {
+        args.port.clone()
+    };
+
+    info!("open port {:?} with rate of {}", port_name, args.baud_rate.unwrap());
+    let port = SerialPortBuilder::new()
+        .baud_rate(args.baud_rate.unwrap())
+        .open(port_name.unwrap())?;
+
+    Ok(port)
+}
+
+
+fn main() -> Result<()> {
+    env_logger::init();
     let args = Args::parse();
 
-    let port = SerialPortBuilder::new().baud_rate(args.baud_rate.unwrap()).open(args.port.clone().unwrap()).unwrap();
-    let port = Arc::new(Mutex::new(port));
+    let port = open_serial_port(&args)?;
+    let port_arc = Arc::new(Mutex::new(port));
+
+    let port_arc_clone = port_arc.clone();
 
     // Spawn a thread to read from stdin and write to the serial port.
-    let port_clone = args.port.clone();
-    std::thread::spawn({
-        let port = port.clone();
-        move || {
-            if let Err(()) = read_stdin_loop(port, &port_clone.unwrap()) {
-                std::process::exit(1);
-            }
+    std::thread::spawn(move || {
+        if let Err(_) = read_stdin_loop(port_arc_clone, "port_name_placeholder") {
+            std::process::exit(1);
         }
     });
 
     // Read from serial port and write to stdout in the main thread.
-    read_serial_loop(port, &args.port.unwrap())?;
+    read_serial_loop(port_arc, "port_name_placeholder")?;
 
     Ok(())
 }
@@ -57,7 +97,7 @@ fn read_stdin_loop(port: Arc<Mutex<SerialPort>>, port_name: &str) -> Result<(), 
     }
 }
 
-fn read_serial_loop(port: Arc<Mutex<SerialPort>>, port_name: &str) -> Result<(), ()> {
+fn read_serial_loop(port: Arc<Mutex<SerialPort>>, port_name: &str) -> Result<()> {
     let mut buffer = [0; 512];
     loop {
         let mut port = port.lock().unwrap();
@@ -66,19 +106,13 @@ fn read_serial_loop(port: Arc<Mutex<SerialPort>>, port_name: &str) -> Result<(),
             Ok(n) => {
                 std::io::stdout()
                     .write_all(&buffer[..n])
-                    .map_err(|e| eprintln!("Error: Failed to write to stdout: {}", e))?;
+                    .context("Failed to write to stdout")?
             }
             Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => continue,
             Err(e) => {
                 eprintln!("Error: Failed to read from {}: {}", port_name, e);
-                return Err(());
+                bail!("");
             }
         }
-    }
-}
-
-fn main() {
-    if let Err(()) = do_main() {
-        std::process::exit(1);
     }
 }
