0fb23595a026e1bc2d20f20e6f272d358c787d0c
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2024-01-03 22:37:44 +0200

    change library to serialport

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,84 @@
 use std::io::{Read, Write};
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
+use serialport::{SerialPort, SerialPortBuilder};
+use clap::Parser;
 
-use serial2::SerialPort;
 
-fn main() -> Result<(), ()> {
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// port name (eg. COM23 OR /dev/ttyUSB0)
+    #[arg(short, long)]
+    port: Option<String>,
 
-	let args: Vec<_> = std::env::args().collect();
-	if args.len() != 3 {
-		let prog_name = args[0].rsplit_once('/').map(|(_parent, name)| name).unwrap_or(&args[0]);
-		eprintln!("Usage: {} PORT BAUD", prog_name);
-		return Err(());
-	}
-
-	let port_name = &args[1];
-	let baud_rate: u32 = args[2]
-		.parse()
-		.map_err(|_| eprintln!("Error: invalid baud rate: {}", args[2]))?;
-
-	let port = SerialPort::open(port_name, baud_rate)
-		.map_err(|e| eprintln!("Error: Failed to open {}: {}", port_name, e))?;
-	let port = Arc::new(port);
-    port.set_dtr(true).expect("Failed set terminal state");
-    port.set_rts(true).expect("Failed set rts");
-	// Spawn a thread to read from stdin and write to the serial port.
-	std::thread::spawn({
-		let port = port.clone();
-		let port_name = port_name.to_owned();
-		move || {
-			if let Err(()) = read_stdin_loop(port, &port_name) {
-				std::process::exit(1);
-			}
-		}
-	});
-
-	// Read from serial port and write to stdout in main thread.
-	read_serial_loop(port, port_name)?;
-
-	Ok(())
+    /// port name (eg. COM23 OR /dev/ttyUSB0)
+    #[arg(short, long)]
+    baud_rate: Option<u32>,
 }
 
-fn read_stdin_loop(port: Arc<SerialPort>, port_name: &str) -> Result<(), ()> {
-	let stdin = std::io::stdin();
-	let mut stdin = stdin.lock();
-	let mut buffer = [0; 512];
-	loop {
-		let read = stdin
-			.read(&mut buffer)
-			.map_err(|e| eprintln!("Error: Failed to read from stdin: {}", e))?;
-		if read == 0 {
-			return Ok(());
-		} else {
-			port.write(&buffer[..read])
-				.map_err(|e| eprintln!("Error: Failed to write to {}: {}", port_name, e))?;
-		}
-	}
+fn do_main() -> Result<(), ()> {
+    let args = Args::parse();
+
+    let port = SerialPortBuilder::new().baud_rate(args.baud_rate.unwrap()).open(args.port.clone().unwrap()).unwrap();
+    let port = Arc::new(Mutex::new(port));
+
+    // Spawn a thread to read from stdin and write to the serial port.
+    let port_clone = args.port.clone();
+    std::thread::spawn({
+        let port = port.clone();
+        move || {
+            if let Err(()) = read_stdin_loop(port, &port_clone.unwrap()) {
+                std::process::exit(1);
+            }
+        }
+    });
+
+    // Read from serial port and write to stdout in the main thread.
+    read_serial_loop(port, &args.port.unwrap())?;
+
+    Ok(())
 }
 
-fn read_serial_loop(port: Arc<SerialPort>, port_name: &str) -> Result<(), ()> {
-	let mut buffer = [0; 512];
-	loop {
-		match port.read(&mut buffer) {
-			Ok(0) => return Ok(()),
-			Ok(n) => {
-				std::io::stdout()
-					.write_all(&buffer[..n])
-					.map_err(|e| eprintln!("Error: Failed to write to stdout: {}", e))?;
-			},
-			Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => {
-                println!("Timeout");
-                continue;
-            },
-			Err(e) => {
-				eprintln!("Error: Failed to read from {}: {}", port_name, e);
-				return Err(());
-			},
-		}
-	}
+fn read_stdin_loop(port: Arc<Mutex<SerialPort>>, port_name: &str) -> Result<(), ()> {
+    let stdin = std::io::stdin();
+    let mut stdin = stdin.lock();
+    let mut buffer = [0; 512];
+    loop {
+        let read = stdin
+            .read(&mut buffer)
+            .map_err(|e| eprintln!("Error: Failed to read from stdin: {}", e))?;
+        if read == 0 {
+            return Ok(());
+        } else {
+            let mut port = port.lock().unwrap();
+            port.write(&buffer[..read])
+                .map_err(|e| eprintln!("Error: Failed to write to {}: {}", port_name, e))?;
+        }
+    }
+}
+
+fn read_serial_loop(port: Arc<Mutex<SerialPort>>, port_name: &str) -> Result<(), ()> {
+    let mut buffer = [0; 512];
+    loop {
+        let mut port = port.lock().unwrap();
+        match port.read(&mut buffer) {
+            Ok(0) => return Ok(()),
+            Ok(n) => {
+                std::io::stdout()
+                    .write_all(&buffer[..n])
+                    .map_err(|e| eprintln!("Error: Failed to write to stdout: {}", e))?;
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => continue,
+            Err(e) => {
+                eprintln!("Error: Failed to read from {}: {}", port_name, e);
+                return Err(());
+            }
+        }
+    }
+}
+
+fn main() {
+    if let Err(()) = do_main() {
+        std::process::exit(1);
+    }
 }
